6d330b25ecf45e84b1a6f16093573181415091fd
vpidkivka <vpidkivka@eliteit.org>
2020-10-26 01:00:36 +0200

    Updates

---
+++ precache-manifest.25ebf1644607dbe9dd1582fa0229333b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6e99828f15217178939e8b5ed742420",
+    "url": "/dog_app/index.html"
+  },
+  {
+    "revision": "5ecc53ccb9d9641fa89b",
+    "url": "/dog_app/static/css/main.0fb0a215.chunk.css"
+  },
+  {
+    "revision": "60f8ce40544293beff37",
+    "url": "/dog_app/static/js/2.26fd98bc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/dog_app/static/js/2.26fd98bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ecc53ccb9d9641fa89b",
+    "url": "/dog_app/static/js/main.85369036.chunk.js"
+  },
+  {
+    "revision": "dfe8a81eab18e7b5c5ff",
+    "url": "/dog_app/static/js/runtime-main.3091044b.js"
+  },
+  {
+    "revision": "75fa68d4d95a95385dabee5a76f50797",
+    "url": "/dog_app/static/media/dog.75fa68d4.mp3"
+  }
+]);
